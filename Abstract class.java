e9b83cecbd562c6aa05d74b78f9e1da96d804f52
Nipashree <146835960+Nipashree@users.noreply.github.com>
2025-04-21 23:28:49 +0530

    Abstract class.java

--- Abstract class.java
+++ Abstract class.java
@@ -1,6 +1,6 @@
 abstract class Language {
 
-  // method of abstract class
+  
   public void display() {
     System.out.println("This is Java Programming");
   }
@@ -10,11 +10,9 @@ class Main extends Language {
 
   public static void main(String[] args) {
     
-    // create an object of Main
+    
     Main obj = new Main();
 
-    // access method of abstract class
-    // using object of Main class
     obj.display();
   }
 }
